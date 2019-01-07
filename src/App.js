4f46c0f4c1a7763f0b13143f7a0f7a31a9150073
DMuttz <dmutter1987@gmail.com>
2019-01-07 00:03:42 -0600

    made changes, not finished yet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import QuoteGenerator from './QuoteGenerator';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello World!</h1>
-      </div>
-    );
+
+    <div className = "App">
+      <QuoteGenerator /> 
+    </div> 
+    )
   }
 }
-
 export default App;
